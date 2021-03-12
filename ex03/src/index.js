854dbb5bc8e5201eb3450f75101b288281b53d71
enida <enida_mujezin_16@yahoo.com>
2021-03-12 12:10:12 +0100

    \n\

---
+++ ex03/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    var myString;
+
+    myString="FirstLine\n\t\\SecondLine\nThirdLine"
+
+    
+    
+
+    
+return {
+        myString
+    }
+    
+        
+    }
+            console.log(main());
+            module.exports = main
