ae058d70751f3b9f670135b611f6cdf7ab61eaaa
enida <enida_mujezin_16yahoo.com>
2021-03-12 22:41:55 +0100

    +

---
+++ ex04/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    var myString;
+
+    myString = "This is my first string." + "This is my second String.";
+
+    return myString;
+        
+
+
+    
+}
+
+console.log(main());
+
+module.exports = main;
