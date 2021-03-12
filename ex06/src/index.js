e6e4e8dba5c6dfae78bf31acbdda6d79309b18b7
enida <enida_mujezin_16yahoo.com>
2021-03-12 22:46:22 +0100

    + my name

---
+++ ex06/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+   
+    var myName = "Enida";
+   
+    var myString;
+   
+   
+    myString = "My name is " + myName + " and i am well."; 
+
+
+    return myString;
+}
+
+console.log(main());
+
+module.exports = main;
