6e57fab87ee6caca5ffc60ad1af7e0da3fc1170c
enida <enida_mujezin_16yahoo.com>
2021-03-12 22:53:42 +0100

    Doe

---
+++ ex09/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+    
+    var lastName ="Doe"
+    
+    
+    var firstLetterofLastName = ""
+
+    firstLetterofLastName = lastName[0];
+
+
+    return firstLetterofLastName;
+}
+
+console.log(main());
+
+module.exports = main;
