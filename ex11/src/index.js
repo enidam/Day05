7996f8f8c0f83212276ab5a9218589414186480f
enida <enida_mujezin_16yahoo.com>
2021-03-12 22:58:29 +0100

    Tesla

---
+++ ex11/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    
+    var lastName = "Tesla";
+    
+    ž
+    var thirdLetterOfLastName = lastName[2];
+
+
+    return thirdLetterOfLastName;
+}
+
+console.log(main());
+
+module.exports = main;
