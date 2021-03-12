8449fdc85d39dde8f9bc99a46ee3440405c7b85e
enida <enida_mujezin_16@yahoo.com>
2021-03-12 10:37:12 +0100

    Double and single

---
+++ ex02/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    var myLink;
+
+    myLink = '<a href = "http://www.example.com" target="_blank">Link</a>'
+    
+
+    
+return {
+        myLink
+    };
+    
+        
+    }
+            console.log(main());
+            module.exports = main
