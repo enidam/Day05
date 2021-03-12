a1b8750c4ffbaf5f5cb309d1744bcb0b8d96db80
enida <enida_mujezin_16yahoo.com>
2021-03-12 22:44:34 +0100

    +=

---
+++ ex05/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+    var myString = "Concatenation is the process of appending"
+
+
+
+    myString += "one string to the end of another string."
+
+
+    
+    return myString;
+   
+}
+
+console.log(main());
+
+module.exports = main;
