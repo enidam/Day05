654e979041d544343bab40de1d048d084413324b
enida <enida_mujezin_16@yahoo.com>
2021-03-12 10:17:10 +0100

    Padawan

---
+++ ex01/src/index.js
@@ -0,0 +1,18 @@
+function main() {
+    var myFirstString;
+    var mySecondString;
+
+    myFirstString = "I think therfore I \"am\"  -- sentiment."
+    mySecondString = "No, you only \"think\" , so you aren't."
+    
+
+    
+return {
+        myFirstString,
+        mySecondString
+    };
+    
+        
+    }
+            console.log(main());
+            module.exports = main
